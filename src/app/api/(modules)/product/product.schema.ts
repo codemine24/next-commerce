00d81b1688495b1931ed6fad046979f037102af3
codemine24 <codemine24@gmail.com>
2025-09-02 12:18:33 +0600

    update add product service function

--- src/app/api/(modules)/product/product.schema.ts
+++ src/app/api/(modules)/product/product.schema.ts
@@ -1,57 +1,61 @@
 import z from "zod";
 
 const addProduct = z.object({
-  body: z.object({
-    name: z
-      .string({ error: "Product name is required" })
-      .min(1, "Product name is required"),
-    model: z.string({ error: "Model should be a text" }).optional(),
-    brand_id: z.uuid({ error: "Brand id should be a valid uuid" }).optional(),
-    size: z.string({ error: "Size should be a text" }).optional(),
-    color: z.string({ error: "Color should be a text" }).optional(),
-    tags: z
-      .array(z.string({ error: "Tags should be a text" }), {
-        error: "Tags should be an array of strings",
-      })
-      .optional(),
-    product_code: z
-      .string({ error: "Product code should be a text" })
-      .optional(),
-    warranty: z.string({ error: "Warranty should be a text" }).optional(),
-    stock: z
-      .number({ error: "Stock should be a positive number" })
-      .int({ error: "Stock should not be a fraction" })
-      .nonnegative({ error: "Stock should not be negative" })
-      .optional(),
-    price: z
-      .number({ error: "Price is required" })
-      .nonnegative({ error: "Price should not be negative" }),
-    discount_price: z
-      .number({ error: "Discount price should be a positive number" })
-      .nonnegative({ error: "Discount price should not be negative" })
-      .optional(),
-    thumbnail: z
-      .string({ error: "Thumbnail should be a valid url" })
-      .optional(),
-    gallery: z
-      .array(z.string({ error: "Gallery item should be a valid url" }), {
-        error: "Gallery should be an array of valid urls",
-      })
-      .optional(),
-    description: z.string({ error: "Description should be a text" }).optional(),
-    specification: z
-      .string({ error: "Specification should be a text" })
-      .optional(),
-    additional_information: z
-      .string({ error: "Additional information should be a text" })
-      .optional(),
-    key_features: z
-      .array(z.string({ error: "Key feature should be a text" }), {
-        error: "Key features should be an array of strings",
-      })
-      .optional(),
-    video_url: z.url({ error: "Video url should be a valid url" }).optional(),
-  }),
+  body: z
+    .object({
+      name: z
+        .string({ error: "Product name is required" })
+        .min(1, "Product name is required"),
+      model: z.string({ error: "Model should be a text" }).optional(),
+      brand_id: z.uuid({ error: "Brand id should be a valid uuid" }).optional(),
+      size: z.string({ error: "Size should be a text" }).optional(),
+      color: z.string({ error: "Color should be a text" }).optional(),
+      tags: z
+        .array(z.string({ error: "Tags should be a text" }), {
+          error: "Tags should be an array of strings",
+        })
+        .optional(),
+      product_code: z
+        .string({ error: "Product code should be a text" })
+        .optional(),
+      warranty: z.string({ error: "Warranty should be a text" }).optional(),
+      stock: z
+        .number({ error: "Stock should be a positive number" })
+        .int({ error: "Stock should not be a fraction" })
+        .nonnegative({ error: "Stock should not be negative" })
+        .optional(),
+      price: z
+        .number({ error: "Price is required" })
+        .nonnegative({ error: "Price should not be negative" }),
+      discount_price: z
+        .number({ error: "Discount price should be a positive number" })
+        .nonnegative({ error: "Discount price should not be negative" })
+        .optional(),
+      thumbnail: z
+        .string({ error: "Thumbnail should be a valid url" })
+        .optional(),
+      gallery: z
+        .array(z.string({ error: "Gallery item should be a valid url" }), {
+          error: "Gallery should be an array of valid urls",
+        })
+        .optional(),
+      description: z
+        .string({ error: "Description should be a text" })
+        .optional(),
+      specification: z
+        .string({ error: "Specification should be a text" })
+        .optional(),
+      additional_information: z
+        .string({ error: "Additional information should be a text" })
+        .optional(),
+      key_features: z
+        .array(z.string({ error: "Key feature should be a text" }), {
+          error: "Key features should be an array of strings",
+        })
+        .optional(),
+      video_url: z.url({ error: "Video url should be a valid url" }).optional(),
+    })
+    .strict(),
 });
 
 export const ProductSchemas = {
--- src/app/api/(modules)/product/product.service.ts
+++ src/app/api/(modules)/product/product.service.ts
@@ -1,11 +1,14 @@
 import { prisma } from "@/app/api/(helpers)/shared/prisma";
+import { slugGenerator } from "../../(helpers)/utils/slug-generator";
 import { ProductPayload } from "./product.interface";
 
 const addProduct = async (payload: ProductPayload) => {
   const productCode = "product-" + Math.random().toString(36).substring(2, 9);
+
   const product = await prisma.product.create({
     data: {
       ...payload,
+      slug: slugGenerator(payload.name),
       product_code: productCode,
     },
   });
@@ -15,7 +18,7 @@ const addProduct = async (payload: ProductPayload) => {
 
 const getProducts = async (query: Record<string, any>) => {
   console.log("query..........", query);
-  
+
   const products = await prisma.product.findMany();
   return products;
 };
@@ -31,9 +34,6 @@ const getProduct = async (id: string) => {
 };
 
 const updateProduct = async (id: string, payload: ProductPayload) => {
-  console.log('id...', id);
-  console.log('payload...', payload);
-  
   const result = await prisma.product.update({
     where: {
       id,
