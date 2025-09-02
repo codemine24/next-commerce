8538ca00c9d036da86e8b1588c7596574937c8d1
codemine24 <codemine24@gmail.com>
2025-09-02 11:24:04 +0600

    modified add product schema

--- src/app/api/(modules)/product/product.schema.ts
+++ src/app/api/(modules)/product/product.schema.ts
@@ -2,25 +2,55 @@ import z from "zod";
 
 const addProduct = z.object({
   body: z.object({
-    name: z.string().min(1, "Product name is required"),
-    slug: z.string().min(1, "Slug is required"),
-    model: z.string().optional(),
-    brand_id: z.string().optional(),
-    size: z.string().optional(),
-    color: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    product_code: z.string().optional(),
-    warranty: z.string().optional(),
-    stock: z.number().int().nonnegative().optional(),
-    price: z.number().int({ message: "Price must be an integer" }),
-    discount_price: z.number().int().optional(),
-    thumbnail: z.string().optional(),
-    gallery: z.array(z.string()).optional(),
-    description: z.string().optional(),
-    specification: z.string().optional(),
-    additional_information: z.string().optional(),
-    key_features: z.array(z.string()).optional(),
-    video_url: z.string().url().optional(),
+    name: z
+      .string({ error: "Product name is required" })
+      .min(1, "Product name is required"),
+    model: z.string({ error: "Model should be a text" }).optional(),
+    brand_id: z.uuid({ error: "Brand id should be a valid uuid" }).optional(),
+    size: z.string({ error: "Size should be a text" }).optional(),
+    color: z.string({ error: "Color should be a text" }).optional(),
+    tags: z
+      .array(z.string({ error: "Tags should be a text" }), {
+        error: "Tags should be an array of strings",
+      })
+      .optional(),
+    product_code: z
+      .string({ error: "Product code should be a text" })
+      .optional(),
+    warranty: z.string({ error: "Warranty should be a text" }).optional(),
+    stock: z
+      .number({ error: "Stock should be a positive number" })
+      .int({ error: "Stock should not be a fraction" })
+      .nonnegative({ error: "Stock should not be negative" })
+      .optional(),
+    price: z
+      .number({ error: "Price is required" })
+      .nonnegative({ error: "Price should not be negative" }),
+    discount_price: z
+      .number({ error: "Discount price should be a positive number" })
+      .nonnegative({ error: "Discount price should not be negative" })
+      .optional(),
+    thumbnail: z
+      .string({ error: "Thumbnail should be a valid url" })
+      .optional(),
+    gallery: z
+      .array(z.string({ error: "Gallery item should be a valid url" }), {
+        error: "Gallery should be an array of valid urls",
+      })
+      .optional(),
+    description: z.string({ error: "Description should be a text" }).optional(),
+    specification: z
+      .string({ error: "Specification should be a text" })
+      .optional(),
+    additional_information: z
+      .string({ error: "Additional information should be a text" })
+      .optional(),
+    key_features: z
+      .array(z.string({ error: "Key feature should be a text" }), {
+        error: "Key features should be an array of strings",
+      })
+      .optional(),
+    video_url: z.url({ error: "Video url should be a valid url" }).optional(),
   }),
 });
 
