90cccc00d7e3f574ac8eab27e656c2be66b25dbd
codemine24 <codemine24@gmail.com>
2025-09-05 00:57:13 +0600

    update brand

--- src/app/api/(modules)/brand/[id]/route.ts
+++ src/app/api/(modules)/brand/[id]/route.ts
@@ -1,6 +1,8 @@
 import { catchAsync } from "@/app/api/(helpers)/shared/catch-async";
 import { successResponse } from "@/app/api/(helpers)/shared/response";
 import payloadValidator from "@/app/api/(helpers)/utils/payload-validator";
+import userAuthenticator from "@/app/api/(helpers)/utils/user-authenticator";
+import { UserRole } from "@prisma/client";
 import httpStatus from "http-status";
 import { NextRequest } from "next/server";
 import { BrandSchemas } from "../brand.schema";
@@ -33,19 +35,22 @@ export const PATCH = catchAsync(
     request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
   ) => {
-    // Step 1: Extract id from route params
+    // Step 1: Authenticate user
+    await userAuthenticator(request, [UserRole.SUPER_ADMIN, UserRole.ADMIN]);
+
+    // Step 2: Extract id from route params
     const id = (await params).id;
 
-    // Step 2: Parse request body
+    // Step 3: Parse request body
     const body = await request.json();
 
-    // Step 3: Validate request body against update brand schema
+    // Step 4: Validate request body against update brand schema
     await payloadValidator(BrandSchemas.updateBrand, body);
 
-    // Step 4: Call service layer to update brand in database
+    // Step 5: Call service layer to update brand in database
     const result = await BrandServices.updateBrand(id, body);
 
-    // Step 5: Return success response with updated brand
+    // Step 6: Return success response with updated brand
     return successResponse({
       statusCode: httpStatus.OK,
       message: "Brand updated successfully",
