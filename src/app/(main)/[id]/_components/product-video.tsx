0143f5439a6529cf64725e678a148ad42321eccf
Forhad Hossain <forhadhossain357471@gmail.com>
2025-09-17 17:41:52 +0600

    Added product video and similar product section in single product page

---
+++ src/app/(main)/[id]/_components/product-video.tsx
@@ -0,0 +1,43 @@
+import Box from "@mui/material/Box";
+import Image from "next/image";
+
+import productVideoThumbnail from "@/../public/assets/product-video-thumbnail.svg";
+
+import { BORDER_RADIUS } from "@/theme";
+
+export const ProductVideo = ({
+  videoUrl,
+}: {
+  videoUrl: string | null | undefined;
+}) => {
+  return (
+    <Box id="product-video">
+      <Box
+        sx={{
+          width: "100%",
+          height: 560,
+          overflow: "hidden",
+          position: "relative",
+          borderRadius: BORDER_RADIUS.default,
+        }}
+      >
+        {videoUrl ? (
+          <iframe
+            src={videoUrl}
+            title="Product Video"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{ width: "100%", height: "100%" }}
+          />
+        ) : (
+          <Image
+            src={productVideoThumbnail}
+            alt="Product Video Thumbnail"
+            fill
+            style={{ objectFit: "cover", objectPosition: "center center" }}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
