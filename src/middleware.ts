2d91837b84e139255745691e11ec60246ae5e63e
RakibMojumder <sujonahmed45a4@gmail.com>
2025-09-18 11:04:11 +0600

    not found issue solved

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,14 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get("access_token")?.value || "";
 
-    const decodedToken = jwtDecode(token, { header: true }) as { role: string };
-
     const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
     const isAccountRoute = request.nextUrl.pathname.startsWith("/account");
+
+    if ((isAccountRoute || isAdminRoute) && !token) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    const decodedToken = jwtDecode(token, { header: true }) as { role: string };
 
     if (isAdminRoute && decodedToken.role !== "ADMIN") {
         return NextResponse.redirect(new URL("/not-found", request.url));
@@ -18,16 +22,14 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/not-found", request.url));
     }
 
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-
     return NextResponse.next();
 }
 
 export const config = {
     matcher: [
+        "/account",
         "/account/:path*",
+        "/admin",
         "/admin/:path*",
     ],
 };
