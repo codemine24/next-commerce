4cc6c2a9da4747bc293a8033ff976e38f963d8de
riyazulhaque64 <riyazulhaque64@gmail.com>
2025-09-25 23:12:14 +0600

    add attributes api

--- src/app/api/(modules)/attribute/route.ts
+++ src/app/api/(modules)/attribute/route.ts
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 
 import { catchAsync } from "../../(helpers)/shared/catch-async";
 import { successResponse } from "../../(helpers)/shared/response";
+import { commonSchemas } from "../../(helpers)/shared/schema";
 import payloadValidator from "../../(helpers)/utils/payload-validator";
 import userAuthenticator from "../../(helpers)/utils/user-authenticator";
 
@@ -28,6 +29,48 @@ export const POST = catchAsync(async (req: NextRequest) => {
   return successResponse({
     statusCode: httpStatus.CREATED,
     message: "Attribute created successfully",
+    data: result,
+  });
+});
+
+// ------------------------------------ GET ALL ATTRIBUTES ------------------------------------
+export const GET = catchAsync(async (req: NextRequest) => {
+  // Step 1: Extract search parameters from the request URL
+  const searchParams = req.nextUrl.searchParams;
+
+  // Step 2: Convert search parameters into a plain object
+  const queryParams = Object.fromEntries(searchParams.entries());
+
+  // Step 3: Fetch attributes from the service layer using query parameters
+  const result = await AttributeServices.getAttributes(queryParams);
+
+  // Step 4: Return success response with attributes and metadata
+  return successResponse({
+    statusCode: httpStatus.OK,
+    message: "Attributes fetched successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+// ------------------------------------ DELETE ATTRIBUTES -------------------------------------
+export const DELETE = catchAsync(async (req: NextRequest) => {
+  // Step 1: Authenticate user
+  await userAuthenticator(req, [UserRole.SUPER_ADMIN, UserRole.ADMIN]);
+
+  // Step 2: Parse request body
+  const body = await req.json();
+
+  // Step 3: Validate request body against schema
+  await payloadValidator(commonSchemas.deleteRecordsValidationSchema, body);
+
+  // Step 3: Delete attributes from the service layer
+  const result = await AttributeServices.deleteAttributes(body);
+
+  // Step 4: Return success response with attributes and metadata
+  return successResponse({
+    statusCode: httpStatus.OK,
+    message: "Attributes deleted successfully",
     data: result,
   });
 });
