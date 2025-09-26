646f5853dbeb8f44811fb14d16e21c53cb1cf397
RakibMojumder <sujonahmed45a4@gmail.com>
2025-09-26 16:26:54 +0600

    build error solved in similar products and new arrivals

--- src/app/(main)/_components/hot-deals.tsx
+++ src/app/(main)/_components/hot-deals.tsx
@@ -1,83 +1,21 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
+import { getProducts } from "@/actions/product";
+import { Product } from "@/interfaces/product";
+
 import { ProductCard } from "../shop/_components/product-card";
 
 import { SectionTitle } from "./section-title";
 
-const Products = [
-  {
-    id: "1",
-    name: "Living Room Furniture Chair",
-    slug: "product-1",
-    thumbnail: "",
-    price: 100,
-    discount_price: 50,
-  },
-  {
-    id: "2",
-    name: "Living Room Furniture Chair",
-    slug: "product-2",
-    thumbnail: "",
-    price: 200,
-    discount_price: 100,
-  },
-  {
-    id: "3",
-    name: "Living Room Furniture Chair",
-    slug: "product-1",
-    thumbnail: "",
-    price: 100,
-    discount_price: 50,
-  },
-  {
-    id: "4",
-    name: "Living Room Furniture Chair",
-    slug: "product-2",
-    thumbnail: "",
-    price: 200,
-    discount_price: 100,
-  },
-  {
-    id: "5",
-    name: "Living Room Furniture Chair",
-    slug: "product-1",
-    thumbnail: "",
-    price: 100,
-    discount_price: 50,
-  },
-  {
-    id: "6",
-    name: "Living Room Furniture Chair",
-    slug: "product-2",
-    thumbnail: "",
-    price: 200,
-    discount_price: 100,
-  },
-  {
-    id: "7",
-    name: "Living Room Furniture Chair",
-    slug: "product-1",
-    thumbnail: "",
-    price: 100,
-    discount_price: 50,
-  },
-  {
-    id: "8",
-    name: "Living Room Furniture Chair",
-    slug: "product-2",
-    thumbnail: "",
-    price: 200,
-    discount_price: 100,
-  },
-];
+export const HotDeals = async () => {
+  const products = await getProducts();
 
-export const HotDeals = () => {
   return (
     <Box component="section" py={5}>
       <SectionTitle title="Hot Deals" href="/shop" />
       <Grid container spacing={2}>
-        {Products.map((product) => (
+        {products?.data?.map((product: Product) => (
           <Grid key={product.id} size={{ xs: 6, md: 4, lg: 3 }}>
             <ProductCard product={product} />
           </Grid>
