4809f41954c95a1539ba4fc16ac275e502e2262e
RakibMojumder <sujonahmed45a4@gmail.com>
2025-09-27 16:11:46 +0600

    error handle in edit product page

--- src/app/(admin-dashboard)/admin/products/edit/[slug]/page.tsx
+++ src/app/(admin-dashboard)/admin/products/edit/[slug]/page.tsx
@@ -1,23 +1,32 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { Suspense } from "react";
 
 import { getBrands } from "@/actions/brand";
 import { getProductBySlug } from "@/actions/product";
+import { ErrorComponent } from "@/components/error-component";
+import { LoadingSpinner } from "@/components/loading-spinner";
 
 import { EditProduct } from "./_components/edit-product";
 
-const EditProductPage = async ({ params }: { params: { slug: string } }) => {
-    const brandsPromise = getBrands();
-    const productPromise = getProductBySlug(params.slug);
+const EditProductPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+    try {
+        const { slug } = await params;
+        const brandsPromise = getBrands();
+        const productPromise = getProductBySlug(slug);
 
-    const [brands, product] = await Promise.all([brandsPromise, productPromise]);
+        const [brands, product] = await Promise.all([brandsPromise, productPromise]);
 
-    return (
-        <Box pb={10}>
-            <Typography variant="h3" mb={4}>Edit Product</Typography>
-            <EditProduct brands={brands.data} product={product.data} />
-        </Box>
-    );
+        return (
+            <Box pb={10}>
+                <Suspense fallback={<LoadingSpinner />}>
+                    <EditProduct brands={brands.data} product={product.data} />
+                </Suspense>
+            </Box>
+        );
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+        return <ErrorComponent message="Sorry, there was an error loading the product data." />;
+    }
 }
 
 export default EditProductPage;
