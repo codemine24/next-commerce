195adea6e9b88ddeeec0934ad981af7cb511b7d9
riyazulhaque64 <riyazulhaque64@gmail.com>
2025-10-22 14:28:58 +0600

    logo is linked to the homepage

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      width={200}
-      height="100%"
-      component={Link}
-      href="/"
-      fontSize={20}
-    >
-      {/* <Image src="/images/logo.svg" alt="Logo" width={150} height={50} /> */}
-      <Box sx={{fontWeight: 900}}>Zero</Box>
-      <Box sx={{fontWeight: 300}}>Commerce</Box>
-    </Box>
+    <Link href="/">
+      <Box
+        display="flex"
+        alignItems="center"
+        width={200}
+        height="100%"
+        component={Link}
+        href="/"
+        fontSize={20}
+      >
+        {/* <Image src="/images/logo.svg" alt="Logo" width={150} height={50} /> */}
+        <Box sx={{ fontWeight: 900 }}>Zero</Box>
+        <Box sx={{ fontWeight: 300 }}>Commerce</Box>
+      </Box>
+    </Link>
   );
 };
