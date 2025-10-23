7ba241cf938db05b4a7976d2c0d0841786ffcced
codemine24 <codemine24@gmail.com>
2025-10-23 15:06:34 +0600

    fix: product card design changed

--- src/app/(main)/shop/_components/buy-now-button.tsx
+++ src/app/(main)/shop/_components/buy-now-button.tsx
@@ -1,20 +1,8 @@
 import { Button } from "@mui/material";
-import React from "react";
-
-import { BORDER_RADIUS } from "@/theme";
 
 const BuyNowButton = () => {
   return (
-    <Button
-      variant="soft"
-      color="primary"
-      sx={{
-        fontSize: ".85rem",
-        borderRadius: BORDER_RADIUS.default,
-        flexGrow: 1,
-        boxShadow: "none",
-      }}
-    >
+    <Button fullWidth variant="contained" color="primary">
       Buy Now
     </Button>
   );
--- src/app/(main)/shop/_components/product-quick-view-button.tsx
+++ src/app/(main)/shop/_components/product-quick-view-button.tsx
@@ -4,33 +4,15 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 
 import { ProductQuickView } from "@/components/product/product-quick-view";
+import { EyeIcon } from "@/icons/eye";
 import { Product } from "@/interfaces/product";
-import { BORDER_RADIUS } from "@/theme";
 
 export const ProductQuickViewButton = ({ product }: { product: Product }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setOpen(true)}
-        sx={{
-          color: "text.primary",
-          fontSize: ".85rem",
-          bgcolor: "rgb(235 236 237)",
-          px: 1,
-          flexGrow: 1,
-          borderRadius: BORDER_RADIUS.default,
-          boxShadow: "none",
-          transition: "all 0.3s",
-          "&:hover": {
-            bgcolor: "primary.dark",
-            color: "common.white",
-          },
-        }}
-      >
-        Quick view
+      <Button variant="soft" color="primary" onClick={() => setOpen(true)}>
+        <EyeIcon />
       </Button>
       <ProductQuickView
         open={open}
