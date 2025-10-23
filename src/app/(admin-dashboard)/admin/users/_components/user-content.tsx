f133317fc68dc7850e234e241772d0e3d93c0e35
Forhad Hossain <forhadhossain357471@gmail.com>
2025-10-23 12:44:29 +0600

    Added answar edit option

--- src/app/(admin-dashboard)/admin/users/_components/user-content.tsx
+++ src/app/(admin-dashboard)/admin/users/_components/user-content.tsx
@@ -7,33 +7,37 @@ import { SearchParams } from "@/interfaces/common";
 
 import { UserTable } from "./user-table";
 
-export const UserContent = async ({ searchParams }: { searchParams: Promise<SearchParams> }) => {
-    const query = await searchParams;
-    const data = await getUsers(query);
+export const UserContent = async ({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) => {
+  const query = await searchParams;
+  const data = await getUsers(query);
 
-    return (
-        <Box>
-            {/* Error Component */}
-            {!data.success && <ErrorComponent message={data.message} />}
+  return (
+    <Box>
+      {/* Error Component */}
+      {!data.success && <ErrorComponent message={data.message} />}
 
-            {/* User Table */}
-            {data.success && (
-                <>
-                    <UserTable users={data.data} />
-                    <Box
-                        p={2}
-                        bgcolor="background.default"
-                        borderTop={1}
-                        borderColor="divider"
-                    >
-                        <Pagination
-                            page={data.meta.page}
-                            total={data.meta.total}
-                            limit={data.meta.limit}
-                        />
-                    </Box>
-                </>
-            )}
-        </Box>
-    )
+      {/* User Table */}
+      {data.success && (
+        <>
+          <UserTable users={data.data} />
+          <Box
+            p={2}
+            bgcolor="background.default"
+            borderTop={1}
+            borderColor="divider"
+          >
+            <Pagination
+              page={data.meta.page}
+              total={data.meta.total}
+              limit={data.meta.limit}
+            />
+          </Box>
+        </>
+      )}
+    </Box>
+  );
 };
